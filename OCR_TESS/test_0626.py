396e57dba13b4858147cbdd315a8336788ec5d1f
Juwon <34915209+zzu0203@users.noreply.github.com>
2019-06-26 14:51:04 +0900

    Create test_0626.py

---
+++ OCR_TESS/test_0626.py
@@ -0,0 +1,56 @@
+#tesseract 결과 field mapping
+import cv2
+import numpy as np
+import os
+import argparse
+import matplotlib.pyplot as plt
+import pytesseract
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filename", required = True)
+    parser.add_argument("--hsize", required = True)
+    args = parser.parse_args()
+    filename = args.filename
+    height_ratio = int(args.hsize) #height = 원본 1200, 1500 등 리사이즈
+
+    img = cv2.imread(filename)
+    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img1 = img_gray.copy()
+    height, width, _ = img.shape
+
+    #height_ratio = height
+
+    if height >= height_ratio and height_ratio != height:
+        resize_width = (width * height_ratio) // height
+        height, width = height_ratio, resize_width
+
+    img1 = cv2.resize(img1 , (width , height))
+
+    #boxes = pytesseract.image_to_boxes(img1, lang = 'kor3+eng', config="--psm 4 --oem 1 -c tessedit_char_whitelist=-01234567890XYZ:@")
+    chars = pytesseract.image_to_string(img1, lang = 'kor3+eng', config="--psm 4 --oem 1 -c tessedit_char_whitelist=-01234567890XYZ:@")
+
+    cnt = 0
+    cnt2 = 0
+
+    for i in range(len(chars)):
+        if chars[i] == ' ':
+            pass
+        else:
+            cnt += 1
+
+
+    print(chars)
+
+
+
+
+
+
+
+
+    #cv2.imshow(filename, img_resize)
+    #cv2.waitKey(0)
+
+if __name__ == '__main__':
+    main()
