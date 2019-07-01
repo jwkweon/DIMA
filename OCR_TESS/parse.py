4a904086f3350103107b508bb6bbf122c8680d43
Juwon <34915209+zzu0203@users.noreply.github.com>
2019-07-02 01:53:44 +0900

    Update parse.py

--- OCR_TESS/parse.py
+++ OCR_TESS/parse.py
@@ -15,7 +15,6 @@ with open(os.path.join('.\\', 'result3.csv'), 'w',  encoding='utf-8', newline=''
     writer.writerow([cateName, clText])
 
 
-
 data = [['사업자등록증'], ['법인사없자'], ['등록번호', '112-81-30811']]
 
 df = pd.DataFrame(data)
@@ -48,107 +47,22 @@ class JamoSeparator():
     def get(self):
         return self.result
 
-a = '권'
-jamos = JamoSeparator(a)
-jamos.run()
-print("jamos: \n> {0}\n".format(jamos.get()))
-a = '마'
+def hangmok_correct(hangmok_list, check_word):
+    max_index = 0
+    max_val = 0
+    for i, k in enumerate(hangmok_list):
+        max_val = max(max_val, Jaccard(k, check_word))
+        if Jaccard(k, check_word) == max_val:
+            max_index = i
 
-ord(a)
-ord(a) - 0xB9C8
-47560 // (16 ** 3)
-(47560 % (16 ** 3)) // (16 ** 2)
-47560 % (16 ** 2) // (16 ** 1)
-47560 % (16 ** 1)
-
-
-cho_list = [i for i, char in enumerate("ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ") if char == 'ㅁ']
-cho_list
-
-def jamo_to_bit(jamo_list):
-    cho = jamo_list[0]
-    jung = jamo_list[1]
-    jong = jamo_list[2]
-
-    cho_index = [i for i, char in enumerate("ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ") if char == cho]
-    jung_index = [i for i, char in enumerate("ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ") if char == jung]
-    jong_index = [i for i, char in enumerate(" ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ") if char == jong]
-
-    cho_bit = [0b00010, 0b00011, 0b00100, 0b00101, 0b00110, 0b00111, 0b01000, 0b01001, 0b01010, 0b01011,
-               0b01100, 0b01101, 0b01110, 0b01111, 0b10000, 0b10001, 0b10010, 0b10011, 0b10100]
-    jung_bit = [0b00011, 0b00100, 0b00101, 0b00110, 0b00111, 0b01010, 0b01011, 0b01100, 0b01101, 0b01110,
-                0b01111, 0b10010, 0b10011, 0b10100, 0b10101, 0b10110, 0b10111, 0b11010, 0b11011, 0b11100, 0b11101]
-    jong_bit = [0b00000, 0b00010, 0b00011, 0b00100, 0b00101, 0b00110, 0b00111, 0b01000, 0b01001, 0b01010,
-                0b01011, 0b01100, 0b01101, 0b01110, 0b01111, 0b10000, 0b10001, 0b10011, 0b10100, 0b10101,
-                0b10110, 0b10111, 0b11000, 0b11001, 0b11010, 0b11011, 0b11100, 0b11101]
-
-    result = []
-    result.append([cho_bit[cho_index[0]], jung_bit[jung_index[0]], jong_bit[jong_index[0]]])
-
-    return result
-
-######### 유사도 실험 ######################
-
-a = '대표자'
-sa = []
-
-for i in a:
-    jamos = JamoSeparator(i)
-    jamos.run()
-    sa.append(jamos.get())
-
-#print(sa)
-
-sa_bit = []
-for i in sa:
-    sa_bit.append([k / sum(jamo_to_bit(i)[0]) for k in jamo_to_bit(i)[0]])
-print(sa_bit)
-
-d = '사업자'
-sd = []
-for i in d:
-    print(i)
-    jamos = JamoSeparator(i)
-    jamos.run()
-    sd.append(jamos.get())
-print(sd)
-
-sd_bit = []
-for i in sd:
-    sd_bit.append([k / sum(jamo_to_bit(i)[0]) for k in jamo_to_bit(i)[0]])
-print(sd_bit)
-
-e = '대자'
-se = []
-for i in e:
-    print(i)
-    jamos = JamoSeparator(i)
-    jamos.run()
-    se.append(jamos.get())
-print(se)
-
-se_bit = []
-for i in se:
-    se_bit.append([k / sum(jamo_to_bit(i)[0]) for k in jamo_to_bit(i)[0]])
-print(se_bit)
-
-doc1=np.array(sa_bit)
-doc2=np.array(sd_bit)
-doc3=np.array(se_bit)
-
-print(cos_sim(doc1, np.transpose(doc2))) #문서1과 문서2의 코사인 유사도
-print(cos_sim(doc1, np.transpose(doc3))) #문서1과 문서3의 코사인 유사도
-print(cos_sim(doc2, np.transpose(doc3))) #문서2과 문서3의 코사인 유사도
+    return hangmok_list[max_index]
 
 
 
+hangmok = ['사업자등록증', '법인사업자', '등록번호', '법인명(단체명)', '대표자', '개업년월일',
+           '법인등록번호', '사업장소재지', '본점소재지', '사업의종류', '교부사유', '발급사유',
+           '공동사업자', '사업자단위과세적용사업자여부', '전자세금계산서전용메일주소', '상호',
+           '성명', '주민등록번호', '생년월일', '일반과세자', '개업연월일']
 
-
-
-
-
-
-
-
-
-#
+word = '명'
+hangmok_correct(hangmok_list = hangmok, check_word = word)
